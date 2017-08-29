d731077f9be59b7a3bc770f0387ce62337ef65ba
Vincent Shen <chengwei.shen@EF.com>
2017-08-29 21:15:39 +0800

    ready for signin

--- DifferencesWebClient/src/app/app.module.ts
+++ DifferencesWebClient/src/app/app.module.ts
@@ -1,22 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AuthGuard } from './services/auth.guard';
+import { AuthenticationService } from './services/authentication.service';
+import { IdentityService } from './services/identity.service';
+import { BrowserStorage } from './services/browser-storage.service';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './account/signin/signin.component';
+import { SignupComponent } from './account/signup/signup.component';
+
+// angular2-jwt config for JiT and AoT compilation.
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+// Set tokenGetter to use the same storage in AuthenticationService.Helpers.
+export function getAuthHttp(http: Http) {
+  return new AuthHttp(new AuthConfig({
+      noJwtError: true,
+      tokenGetter: (() => localStorage.getItem("id_token"))
+  }), http);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    Title,
+    AuthGuard,
+    AuthenticationService,
+    IdentityService,
+    BrowserStorage,
+    {
+        provide: AuthHttp,
+        useFactory: getAuthHttp,
+        deps: [Http]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
