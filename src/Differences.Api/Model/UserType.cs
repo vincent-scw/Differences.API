66317bc85eb6b95afb0c5e97ef8ccd324546024a
VincentShen <edentidus@foxmail.com>
2017-10-05 18:37:13 +0800

    user id Int=>Guid

--- src/Differences.Api/Model/UserType.cs
+++ src/Differences.Api/Model/UserType.cs
@@ -11,8 +11,7 @@ namespace Differences.Api.Model
     {
         public UserType()
         {
-            Field(x => x.Id);
-            //Field(x => x.GlobalId);
+            Field("id", x => x.Id.ToString());
             Field(x => x.Email);
             Field(x => x.DisplayName);
             Field(x => x.AvatarUrl);
