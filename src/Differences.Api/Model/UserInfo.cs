2e388ef98a82aebe2ae8b1444774862fa21604d5
Vincent Shen <chengwei.shen@EF.com>
2017-10-09 17:33:00 +0800

    fix issue, use objectid

--- src/Differences.Api/Model/UserInfo.cs
+++ src/Differences.Api/Model/UserInfo.cs
@@ -8,7 +8,7 @@ namespace Differences.Api.Model
 {
     public class UserInfo
     {
-        private const string Claims_ObjectId = "aud";
+        private const string Claims_ObjectId = "oid";
         private const string Claims_Name = "name";
         private const string Claims_Emails = "emails";
 
