a1d94aac14dd50403cc74b8ece1cc43dc648ef6a
Vincent Shen <chengwei.shen@EF.com>
2017-12-07 12:45:42 +0800

    add migration scripts

---
+++ src/Differences.DataAccess/Migrations/20171207044400_RemoveArticle.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Differences.DataAccess.Migrations
+{
+    public partial class RemoveArticle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "ArticleUpdateHistories");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ArticleId",
+                table: "Comments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AuthorId = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(nullable: true),
+                    LastUpdatedBy = table.Column<Guid>(nullable: true),
+                    Tags = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleUpdateHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ArticleId = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    LastUpdateTime = table.Column<DateTime>(nullable: true),
+                    LastUpdatedBy = table.Column<Guid>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleUpdateHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArticleUpdateHistories_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ArticleId",
+                table: "Comments",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleUpdateHistories_ArticleId",
+                table: "ArticleUpdateHistories",
+                column: "ArticleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
