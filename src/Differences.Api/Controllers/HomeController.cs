d9b6e17faaa4eafc8b4977e113aa013726e065c0
Vincent Shen <edentidus@foxmail.com>
2017-12-12 17:45:09 +0800

    delete unnecessary files

---
+++ src/Differences.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Differences.Api.Controllers
+{
+    [Route("")]
+    public class HomeController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "I am working...";    
+        }
+    }
+}
