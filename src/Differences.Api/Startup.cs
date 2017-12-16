c3cd6462aa383ee9017f907fa3bfd85b0de686c9
Vincent Shen <chengwei.shen@EF.com>
2017-12-16 09:54:09 +0800

    handle 404

--- src/Differences.Api/Startup.cs
+++ src/Differences.Api/Startup.cs
@@ -98,6 +98,19 @@ namespace Differences.Api
             app.UseAuthentication();
             app.UseMiddleware<GraphQLMiddleware>(new GraphQLSettings());
 
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !System.IO.Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseMvc();
         }
